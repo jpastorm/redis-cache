f13285a022144234f57214583e20d4df70adab44
jpastorm <josepastormendoza32@gmail.com>
2021-07-11 19:33:37 -0500

    HGet,HSet,HMGet

--- cache/storage/storage.go
+++ cache/storage/storage.go
@@ -38,3 +38,34 @@ func (s storage) Get(key string) (string, error) {
 	}
 	return result, nil
 }
+// - HSet("myhash", map[string]interface{}{"key1": "value1", "key2": "value2"})
+func (s storage) HSet(key string, value map[string]interface{}) error {
+	err := s.client.HMSet(s.context, key, value).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s storage) HGet(key ,field string) (string, error) {
+	result, err := s.client.HGet(s.context, key, field).Result()
+	if err == redis.Nil {
+		return result, errors.New(key + " or " + field + " doesnt exists")
+	}
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func (s storage) HMGet(key string , fields []string) ([]interface{}, error) {
+	result, err := s.client.HMGet(s.context, key, fields...).Result()
+	if err == redis.Nil {
+		return result, errors.New(key + " or fields doesnt exists")
+	}
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
--- cache/usecase/cache.go
+++ cache/usecase/cache.go
@@ -10,4 +10,7 @@ type Usecase interface {
 type Storage interface {
 	Set(key string, value interface{}) error
 	Get(key string) (string, error)
+	HSet(key string, value map[string]interface{}) error
+	HGet(key ,field string) (string, error)
+	HMGet(key string , fields []string) ([]interface{}, error)
 }
